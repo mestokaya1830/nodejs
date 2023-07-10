eabae31b066f1cd021ddf157510bcd4220483730
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-07-10 00:25:20 -0400

    fvf

---
+++ resize-images/pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div class="section">
+  <div class="container">
+    <resize />
+  </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data () {
+    return {
+
+    }
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+}
+</script>
