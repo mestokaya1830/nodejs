644f5489fb68d3e16bf5c0dae1ec517a24acd4b5
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-08-13 05:16:55 -0400

    nh

--- bundle/app.js
+++ bundle/app.js
@@ -1,11 +1,11 @@
 //npm i -g pkg
 //pkg app.js for all executable
-//pkg app.js --target node18-linux-x64
+//pkg app.js --target node20-linux-x64
 //pkg app.js --target node18-macos-x64
 //pkg app.js --target node18-win-x64
 
-const express = require('express')
-const cors = require('cors')
+ import express from 'express'
+import cors from 'cors'
 const app = express()
 
 app.get('/', (req, res) => {
