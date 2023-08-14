6e3054d6c1a56367e5c80e8601a15b34d304a5e9
mestokaya1830 <123832169+mestokaya1830@users.noreply.github.com>
2023-08-14 01:55:37 -0400

    Update express.js

--- 0-server/http-server/express.js
+++ 0-server/http-server/express.js
@@ -1,6 +1,6 @@
-const express = require('express')
+import express from 'express'
+import cors from 'cors'
 const app = express()
-const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
