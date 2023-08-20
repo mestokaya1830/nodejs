2bbdd1bd895611b32393171aff6ed4a0638daa93
mestokaya <mesto1830@outlook.com>
2023-08-20 07:06:46 -0400

    lol

--- bundle/app.js
+++ bundle/app.js
@@ -1,12 +1,8 @@
-//npm i -g pkg
-//pkg app.js for all executable
-//pkg app.js --target node20-linux-x64
-//pkg app.js --target node18-macos-x64
-//pkg app.js --target node18-win-x64
-
- import express from 'express'
+import express from 'express'
 import cors from 'cors'
 const app = express()
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello!')
