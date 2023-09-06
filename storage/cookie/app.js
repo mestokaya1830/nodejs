4f7e6bbc12bfb8eb53cab8db9c13f7a2b10879c2
mestokaya <mesto1830@outlook.com>
2023-09-06 13:53:27 -0400

    mk

--- storage/cookie/app.js
+++ storage/cookie/app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 app.use(cors())
 app.use(express.json())
