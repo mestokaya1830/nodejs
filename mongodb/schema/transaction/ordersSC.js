8a9905d2780a673920a4343e8615e4555821a344
mestokaya <mesto1830@outlook.com>
2023-10-29 23:43:27 +0100

    vffvfvf

--- mongodb/schema/transaction/ordersSC.js
+++ mongodb/schema/transaction/ordersSC.js
@@ -3,9 +3,9 @@ import mongoose from 'mongoose'
 const ordersSchema = new mongoose.Schema({
   customerID: mongoose.SchemaTypes.ObjectId(),//come from customers-table
   productID: mongoose.SchemaTypes.ObjectId(),//come from products-table
-  products:[{type: String, required: true}],
-  quantity:[{type: Number, required: true}],
-  amount:[{type: Number, required: true}],
+  products:{type: String, required: true},
+  quantity:{type: Number, required: true},
+  amount:{type: Number, required: true},
   created_at:{type: Date, required: true, default:Date.now()}
 })
 
