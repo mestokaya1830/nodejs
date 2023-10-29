978131c3535ec3db2af0fd294fe0149fdeb68c1c
mestokaya <mesto1830@outlook.com>
2023-10-29 23:59:59 +0100

    bgbg

--- mongodb/base/transaction.js
+++ mongodb/base/transaction.js
@@ -1,17 +1,17 @@
 import express from 'express'
 const app = express()
 import cors from 'cors'
-import db from '../db.js'
-import Users from '../usersSchema.js'
+import db from '../schema/transaction/db.js'
+import Customers from '../schema/transaction/customersSC.js'
+import Products from '../schema/transaction/productsSC.js'
+import Orders from '../schema/transaction/ordersSC.js'
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.get('/', async(req, res) => {
-  const result = await Users.find(
-    {scores:{$elemMatch:{$lt: 40}}}
-  )
+  const result = await Customers.find()
   res.json(result)
 })
 
--- mongodb/schema/transaction/ordersSC.js
+++ mongodb/schema/transaction/ordersSC.js
@@ -1,8 +1,8 @@
 import mongoose from 'mongoose'
 
 const ordersSchema = new mongoose.Schema({
-  customerID: mongoose.SchemaTypes.ObjectId(),//come from customers-table
-  productID: mongoose.SchemaTypes.ObjectId(),//come from products-table
+  customerID: mongoose.Schema.Types.ObjectId,//come from customers-table
+  productID: mongoose.Schema.Types.ObjectId,//come from products-table
   products:{type: String, required: true},
   quantity:{type: Number, required: true},
   amount:{type: Number, required: true},
